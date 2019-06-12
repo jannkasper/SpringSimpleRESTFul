b16de64e73e0df50b42b6d8b51956366a74b3a42
Jan K <jann.kasper@gmail.com>
2019-06-12 20:34:02 +0200

    mapper/service tests

--- src/main/java/jannkasper/spring/api/v1/mapper/UserMapper.java
+++ src/main/java/jannkasper/spring/api/v1/mapper/UserMapper.java
@@ -2,13 +2,12 @@ package jannkasper.spring.api.v1.mapper;
 
 import jannkasper.spring.api.v1.model.UserDTO;
 import jannkasper.spring.domain.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.MapperConfig;
 
-@Mapper
+@MapperConfig
 public interface UserMapper {
 
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+//    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserDTO userToUserDTO (User user);
 
