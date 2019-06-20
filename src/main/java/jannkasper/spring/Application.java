e8d48dc9ea172abe4a7dbd6b665c0a22d1557448
Jan K <jann.kasper@gmail.com>
2019-06-20 08:34:50 +0200

    local copy

--- src/main/java/jannkasper/spring/Application.java
+++ src/main/java/jannkasper/spring/Application.java
@@ -3,6 +3,7 @@ package jannkasper.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+///test
 @SpringBootApplication
 public class Application {
 
